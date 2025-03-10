52e32dada0cdf0f14dc126d93dcbf612c93f6839
kangmantul <99028072+kangmantul@users.noreply.github.com>
2025-03-10 12:58:29 +0700

    Shooting Function
    Shooting

--- Assets/Apps/Scripts/Bullet.cs
+++ Assets/Apps/Scripts/Bullet.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour, IPoolObject
 {
-
-    void Update()
-    {
-        
-    }
+    private float speed = 10f;
+    private Vector3 dir;
+    
     public void Activate()
     {
         gameObject.SetActive(true);
@@ -28,5 +26,21 @@ public class Bullet : MonoBehaviour, IPoolObject
     internal void SetRotation(Quaternion rotation)
     {
         transform.rotation = rotation;
+    }
+
+    public void Fire(Vector3 fireDirection)
+    {
+        dir = fireDirection.normalized;
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        if (rb != null)
+        {
+            rb.velocity = dir * speed;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ObjectPool pool = FindObjectOfType<ObjectPool>();
     }
 }
--- Assets/Apps/Scripts/Weapon.cs
+++ Assets/Apps/Scripts/Weapon.cs
@@ -2,35 +2,78 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Weapon : MonoBehaviour
 {
+    private Camera Cam;
+    private Vector3 mouse;
+
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canFire;
+    
     [SerializeField] private float attackDelay;
     private ObjectPool pool;
+    private PlayerInput playerInput;
+    private InputAction fireAction;
 
     private void Awake()
     {
         pool = FindObjectOfType<ObjectPool>();
+        playerInput = GetComponent<PlayerInput>();
+        fireAction = playerInput.actions["Fire"];
     }
 
     private void Start()
     {
+        Cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         StartCoroutine(AttackEnumerator());
     }
 
+    private void Update()
+    {
+        direction();
+    }
     private IEnumerator AttackEnumerator()
     {
         while (true)
         {
-            Shoot();
-            yield return new WaitForSeconds(attackDelay);
+            if (fireAction.triggered && canFire)
+            {
+                Shoot();
+                yield return new WaitForSeconds(attackDelay);
+            }
+            yield return null;
         }
+    }
+
+    public void direction()
+    {
+        mouse = Cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 rotation = mouse - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+
+        transform.rotation= Quaternion.Euler(0, 0, rotZ);
     }
 
     public void Shoot()
     {
-        Bullet b = pool.Request<Bullet>();
-        b.SetPosition(transform.position);
-        b.SetRotation(transform.rotation);
+        //Bullet b = pool.Request<Bullet>();
+        //b.SetPosition(transform.position);
+        //b.SetRotation(transform.rotation);
+
+        if (pool!=null)
+        {
+            Bullet b = pool.Request<Bullet>();
+
+            if (b != null)
+            {
+                b.SetPosition(transform.position);
+                b.SetRotation(transform.rotation);
+                Vector3 direction = transform.up;
+                b.Fire(direction);
+            }
+        }
     }
 }
