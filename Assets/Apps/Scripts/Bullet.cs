621760fda535f9ef91afab13914b1dab0d3dd82e
MKD20590 <michelleandreas123@gmail.com>
2025-03-10 12:13:54 +0700

    shooting
    shooting automatically towards mouse position

--- Assets/Apps/Scripts/Bullet.cs
+++ Assets/Apps/Scripts/Bullet.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Bullet : MonoBehaviour, IPoolObject
 {
-
+    private Vector3 position;
     void Update()
     {
-        
+        transform.position += position * Time.deltaTime * 3;
     }
     public void Activate()
     {
@@ -22,7 +23,8 @@ public class Bullet : MonoBehaviour, IPoolObject
 
     internal void SetPosition(Vector3 position)
     {
-        transform.position = position;
+        //transform.position = position;
+        this.position = new Vector3((position - transform.position).normalized.x, (position - transform.position).normalized.y, 0);
     }
 
     internal void SetRotation(Quaternion rotation)
