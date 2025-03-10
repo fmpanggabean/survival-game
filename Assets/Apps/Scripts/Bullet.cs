30a4a4006ce91c5296a51ba8c285030ff747602b
NasiMentega <rafaelww05@gmail.com>
2025-03-10 12:47:32 +0700

    Update Shooting Feature
    Make the bullet direction at the cursor

--- Assets/Apps/Scripts/Bullet.cs
+++ Assets/Apps/Scripts/Bullet.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour, IPoolObject
 {
+    [SerializeField] private float speed = 10f;
+    private Vector2 direction;
 
     void Update()
     {
-        
+        transform.position += (Vector3)direction * speed * Time.deltaTime;
     }
+
     public void Activate()
     {
         gameObject.SetActive(true);
@@ -28,5 +31,10 @@ public class Bullet : MonoBehaviour, IPoolObject
     internal void SetRotation(Quaternion rotation)
     {
         transform.rotation = rotation;
+    }
+
+    public void SetDirection(Vector2 newDirection)
+    {
+        direction = newDirection.normalized;
     }
 }
